6d343f7e5f967d6f48ca934b9d51afcf9e44320b
dusty-satt <dusty.satt@gmail.com>
2018-07-17 14:15:51 -0500

    Fix notices flooding error logs during schedule importing (#508) (#9)
    Make sure error_message is set before trying to use it in an exception.  Google can respond with non-"OK" responses like "ZERO-RESULTS" which isn't an error and doesn't have an error_message.

--- src/Client/GuzzleAdapter.php
+++ src/Client/GuzzleAdapter.php
@@ -38,7 +38,9 @@ class GuzzleAdapter implements ClientInterface
                 throw new \RuntimeException('Failed to parse response');
             }
 
-            if ($data['status'] !== 'OK') {
+            if ($data['status'] !== 'OK'
+                && isset($data['error_message'])
+            ) {
                 throw new \RuntimeException($data['error_message']);
             }
 
